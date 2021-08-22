7980c4a8064cacbbe64a4b23be604dfde948e487
ljsardosa <ljsardosa@multisyscorp.com>
2021-08-22 15:34:07 +0800

    3:34

--- next.config.js
+++ next.config.js
@@ -10,6 +10,9 @@ module.exports = {
     locales: ["en"],
     defaultLocale: "en",
   },
+  env: {
+    BASE_URL: process.env.BASE_URL,
+  },
   images: {
     domains: ["media.graphcms.com"],
   },
