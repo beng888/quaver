cd37dd28e7fe28bd303d11212b6fded95284b46b
ljsardosa <ljsardosa@multisyscorp.com>
2021-09-04 21:55:49 +0800

    9:55

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import "../styles/globals.css";
 import { GlobalContextWrapper } from "../src/context";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <GlobalContextWrapper>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.svg" />
+      </Head>
       <Component {...pageProps} />
     </GlobalContextWrapper>
   );
