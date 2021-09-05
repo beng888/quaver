1ccd158b6313914d38ea2209845dd8622266599a
ljsardosa <ljsardosa@multisyscorp.com>
2021-09-05 18:56:59 +0800

    6:56

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -9,12 +9,15 @@ import useGlobalContext from "@context/index";
 import { useLocomotiveScroll } from "react-locomotive-scroll";
 import { useEffect } from "react";
 import delivery from "@images/delivery.png";
+import delivery_schedule from "@images/delivery_schedule.png";
+import order_process from "@images/order_process.png";
 
 export default function Layout({ children }) {
   const router = useRouter();
   const { pathname } = router;
-  const { ShowSlider } = useGlobalContext();
-  const [showSlider] = ShowSlider;
+  const { ShowSlider, SliderImages, Slide } = useGlobalContext();
+  const [showSlider, setShowSlider] = ShowSlider;
+  const [, setSliderImages] = SliderImages;
   const { scroll } = useLocomotiveScroll();
 
   const horizontal = pathname === "/gallery/[slug]" || pathname === "/[cakes]";
@@ -22,6 +25,17 @@ export default function Layout({ children }) {
   const Slider = dynamic(() => import("@components/Slider"), {
     loading: () => <b>Loading...</b>,
   });
+
+  const deliverySlide = [
+    {
+      url: delivery_schedule,
+      alt: "delivery_schedule",
+    },
+    {
+      url: order_process,
+      alt: "order_process",
+    },
+  ];
 
   useEffect(() => {
     if (showSlider) scroll?.stop();
@@ -52,7 +66,13 @@ export default function Layout({ children }) {
       {pathname !== "/[cakes]/[slug]" && <Footer pathname={pathname} />}
 
       <div className="fixed left-2 bottom-2">
-        <div className="h-24 w-24 z-50 relative scale-50 transform origin-bottom-left cursor-pointer duration-500 hover:scale-100">
+        <div
+          onClick={() => {
+            setShowSlider(true);
+            setSliderImages(deliverySlide);
+          }}
+          className="h-24 w-24 z-50 relative scale-50 transform origin-bottom-left cursor-pointer duration-500 md:hover:scale-100"
+        >
           <Image src={delivery} alt="delivery" layout="fill" />
         </div>
       </div>
